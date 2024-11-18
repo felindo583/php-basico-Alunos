67ede4daca955327fffdbf3864b4c3caf3f2235c
felindo583 <felindo2203@gmail.com>
2024-11-18 16:37:42 -0300

    php

---
+++ 5_cadastro.php
@@ -0,0 +1,44 @@
+<!-- O programa armazena os usuários em um documento externo com a extensão .txt -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Usuário</title>
+</head>
+<body>
+    <h2>Informe o nome e a senha que deseja cadastrar</h2>
+    <form method="post" action="">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required><br>
+
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" required><br>
+
+        <button type="submit">Cadastrar</button>
+    </form>
+    <?php
+
+    if($_SERVER['REQUEST_METHOD']== 'POST'){
+
+        $nome = $_POST['nome'];
+        $senha =  $_POST['senha'];
+
+        // Abre o arquivo 'usuarios.txt' para escrita
+        // adiciona dados ao final do arquivo
+        $arquivo = fopen('usuarios.txt', 'a');
+
+        // cria uma linha com nome e senha separados por ';'
+        $linha = $nome . ';' . $senha . '\n';
+
+        // escreve no arquivo
+        fwrite($arquivo,$linha);
+
+        // fecha o arquivo
+        fclose($arquivo);
+
+        echo "<p>Usuário Cadastrado com sucesso!</p>";
+    }    
+    ?>
+</body>
+</html>
---
+++ 6_login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login de Usuário</title>
+</head>
+<body>
+    <h1>Faça o login com o usuário cadastrado</h1>
+    <form method="post" action="">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required><br>
+
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" required><br>
+
+        <button type="submit">Entrar</button>
+    </form>
+
+    <?php
+    // Verifica se o formulário foi enviado
+
+
+    if ($_SERVER[ 'REQUEST_METHOD'] == 'POST'){
+      // Recebe os valores enviados pelo formulário
+      $nome = $_POST[ 'nome'];
+      $senha = $_POST['senha'];
+      // Abre o arquivo usuarios.txt para leitura
+
+      $arquivo = fopen( 'usuarios.txt', 'r');
+      $login_sucesso = false;
+
+        // Lê cada linha do arquivo
+        while (($linha = fgets($arquivo)) !== false) {
+         // Divide a linha pelo delimitador ";"
+            list($usuario_arquivo, $senha_arquivo) =    explode(';',    trim($linha));
+            // Verifica se o nome e a senha    correspondem    aos valores no arquivo
+
+            if ($nome == $usuario_arquivo && $senha ==  $senha_arquivo) {
+             $login_sucesso = true;
+            break;
+            }
+        }
+
+       // Fecha o arquivo
+
+        fclose ($arquivo);
+
+
+        // Exibe a mensagem de sucesso ou erro
+        if ($login_sucesso) {
+        echo "<h3>Login realizado com sucessol  Bem-vindo, $nome ! </h3>";
+
+        }else {
+
+        echo
+        "<h3 style='color: red; 'Usuário ou senha incorretos.</h3>";
+        }
+    }
+
+    ?>
+</body>
+</html>
